e09d7108dba7c496ad23ed5541bb42a2b4934d19
LevytskyiS <levytskyi.sv@gmail.com>
2022-12-18 11:51:26 +0100

    Create hw5.py

---
+++ hw5.py
@@ -0,0 +1,27 @@
+from multiprocessing.pool import Pool
+from time import time
+
+
+def factorize(*number: tuple) -> list:
+
+    for num in number:
+        result = [i for i in range(1, num + 1) if not num % i]
+
+    return result
+
+
+if __name__ == '__main__':
+
+    start = time()
+
+    with Pool(processes=2) as pool:
+        a, b, c, d = pool.map(factorize, (128, 255, 99999, 10651060))
+
+    finish = time() - start
+    print(finish)
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316,
+                 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
